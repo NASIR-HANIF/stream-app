a3de25127a245edd7da54ee4d5663124ee16c3f3
nasir hanif <101257760+NASIR-HANIF@users.noreply.github.com>
2024-08-25 09:35:09 +0500

    cors update

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           // Replace 'https://example.com' with your specific origin
-          { key: "Access-Control-Allow-Origin", value: "https://stream-o8rpqnerx-nasir-hanifs-projects.vercel.app" },
+          { key: "Access-Control-Allow-Origin", value: "https://stream-4tal4zrno-nasir-hanifs-projects.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
